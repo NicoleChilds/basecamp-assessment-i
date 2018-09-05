4d58b2c585744cd8b84d703ae59cc2377e4f7ac7
Nicole Childs <nicolechilds@Nicoles-MBP.attlocal.net>
2018-09-04 22:50:50 -0400

    fave number

--- assessment.js
+++ assessment.js
@@ -3,12 +3,11 @@ var 'myName' = "Nicole Childs";
 // CODE HERE
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
-
-// CODE HERE
+var 'faveNum' = 9
 
 // #3 create a variable called 'lovesCode' and set it to true
 
-// CODE HERE
+var 'lovesCode' = true
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
